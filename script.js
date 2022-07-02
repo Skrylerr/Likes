796a8f017fba02219b78dd2503d1427ba61fb175
Skrylerr <P4nZh0p@gmail.com>
2022-07-01 20:49:34 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var likes = 0;
+var likesElement = document.querySelector("#likes");
+
+function addbox1() {
+    likes++;
+    likesElement.innerText = likes + " like(s)";
+}
+
+var likes1 = 0;
+var likes1Element = document.querySelector("#likes1");
+
+function addbox2() {
+    likes1++;
+    likes1Element.innerText = likes1 + " like(s)";
+}
+
+var likes2 = 0;
+var likes2Element = document.querySelector("#likes2");
+
+
+function addbox3() {
+    likes2++;
+    likes2Element.innerText = likes2 + " like(s)";
+}
